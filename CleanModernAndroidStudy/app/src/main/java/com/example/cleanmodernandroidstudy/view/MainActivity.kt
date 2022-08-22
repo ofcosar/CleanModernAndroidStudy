e79dc5cf25849ba5849e60654d06eba810de6bda
ömer faruk coşar <ofcosar1@gmail.com>
2022-08-22 17:04:38 +0300

    mainactivity moved to view package

--- CleanModernAndroidStudy/app/src/main/java/com/example/cleanmodernandroidstudy/view/MainActivity.kt
+++ CleanModernAndroidStudy/app/src/main/java/com/example/cleanmodernandroidstudy/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cleanmodernandroidstudy
+package com.example.cleanmodernandroidstudy.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cleanmodernandroidstudy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
